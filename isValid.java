fcfabf1a4ee6fbb4121678e88fb6b93fc20c11d1
blanksheet <547699302@qq.com>
2018-11-27 19:53:58 +0800

    Add files via upload

---
+++ isValid.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * Created by tianze
+ * 2018/11/27 19:03
+ */
+public class isValid {
+    public static boolean isValid(String s) {
+        if(s.length()==0){
+            return true;
+        }
+
+        if(s.length()%2!=0){
+            return false;
+        }
+
+        Stack<Character> save = new Stack<>();
+        save.push(s.charAt(0));
+        char temp;
+
+        for(int i = 1; i < s.length(); i++){
+
+            if(save.empty()){
+                save.push(s.charAt(i));
+                continue;
+            }
+            else{
+                temp = save.pop();
+            }
+            switch (s.charAt(i)){
+                case '}':
+                    if(temp == '{')
+                      break;
+                    else {
+                        save.push(temp);
+                        save.push(s.charAt(i));
+                        break;
+                    }
+                case ']':
+                    if(temp == '[')
+                      break;
+                    else{
+                        save.push(temp);
+                        save.push(s.charAt(i));
+                        break;
+                    }
+                case ')':
+                    if(temp == '(')
+                      break;
+                    else{
+                        save.push(temp);
+                        save.push(s.charAt(i));
+                        break;
+                    }
+                default:
+                    save.push(temp);
+                    save.push(s.charAt(i));
+            }
+
+        }
+
+        return save.empty();
+    }
+
+
+
+    public static void main(String[] args){
+        String ex1 = "(]";
+        System.out.println(isValid(ex1));
+    }
+}
