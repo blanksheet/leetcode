4b9477ef5e61b8feb8ee6ec2106bb0127ecf1a79
blanksheet <547699302@qq.com>
2018-11-28 10:46:10 +0800

    Add files via upload

--- letterCombinations.java
+++ letterCombinations.java
@@ -9,29 +9,25 @@ import java.util.List;
 public class letterCombinations {
     static String[] strs = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public static List<String> letterCombinations(String digits) {
-        List<String> res = new ArrayList<String>();
+        List<String> result = new ArrayList<String>();
         if (digits == null || digits.length() == 0) {
-            return res;
+            return result;
         }
-        for(char ch: digits.toCharArray()) {
-            int digit = ch - '0';
-            if (digit < 2 || digit > 9) {
-                return res;
-            }
-        }
-        helper(digits, res, new StringBuilder(), 0);
-        return res;
+
+        calculate(digits,result,new StringBuilder(),0);
+        return result;
     }
-    private static void helper(String digits, List<String> list, StringBuilder sb, int index) {
-        if (index == digits.length()) {
-            list.add(sb.toString());
+
+    private static void calculate(String digits, List<String> result,StringBuilder put, int index){
+        if(index == digits.length()){
+            result.add(put.toString());
             return;
         }
-        String chs = strs[digits.charAt(index) - '0'];
-        for (int i = 0; i < chs.length(); i ++) {
-            sb.append(chs.charAt(i));
-            helper(digits, list, sb, index + 1);
-            sb.deleteCharAt(sb.length() - 1);
+
+        for(int i = 0;i < strs[digits.charAt(index) - '0'].length();i++){
+            put.append(strs[digits.charAt(index) - '0'].charAt(i));
+            calculate(digits,result,put,index+1);
+            put.deleteCharAt(put.length()-1);
         }
     }
 
