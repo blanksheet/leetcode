e547e74ce9171b2a49fc166cd8a60eaa408d4c55
Tianze <547699302@qq.com>
2018-12-07 20:40:24 +0800

    Add files via upload

---
+++ 剑指Offer/minNumberInRotateArray.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+/**
+ * Created by tianze
+ * 2018/12/7 20:20
+ */
+public class minNumberInRotateArray {
+    public int minNumberInRotateArray(int [] array) {
+        if(array == null || array.length == 0){
+            return 0;
+        }
+
+        int left = 0;
+        int right = array.length - 1;
+        int res  = 0;
+
+        while(array[left] >= array[right]){
+            if(left == (right -1)){
+                res = right;
+                break;
+            }
+
+            int mid = (right + left)/2;
+
+            //if index of left mid right number is equal then search one by one
+            if(array[left] == array[mid] && array[left] == array[right]){
+                Arrays.sort(array);
+                return array[0];
+            }
+
+            if(array[left] <= array[mid]){
+                left = mid;
+            }
+            else {
+                right = mid;
+            }
+
+        }
+
+        return array[res];
+    }
+}
