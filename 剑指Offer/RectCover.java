99ef1c3d8d330698b4512817e3b5626e0da11ef5
Tianze <547699302@qq.com>
2018-12-10 15:35:25 +0800

    Add files via upload

---
+++ 剑指Offer/RectCover.java
@@ -0,0 +1,29 @@
+/**
+ * Created by tianze
+ * 2018/12/10 15:30
+ */
+public class RectCover {
+    public int RectCover(int target) {
+        if(target == 0){
+            return 0;
+        }
+        if(target == 1){
+            return 1;
+        }
+        if(target == 2){
+            return 2;
+        }
+
+        int first = 1;
+        int second = 2;
+
+        for(int i = 3; i < target; i++){
+            int temp = first + second;
+            first = second;
+            second = temp;
+        }
+
+        return first + second;
+    }
+
+}
