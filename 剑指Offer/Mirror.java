309f0193d18d947e4c52b2fff925c8e3b4dd0949
Tianze <547699302@qq.com>
2018-12-14 16:48:17 +0800

    Add files via upload

---
+++ 剑指Offer/Mirror.java
@@ -0,0 +1,30 @@
+/**
+ * Created by tianze
+ * 2018/12/14 16:18
+ */
+public class Mirror {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+    public void Mirror(TreeNode root) {
+        if(root == null){
+            return;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        Mirror(root.left);
+        Mirror(root.right);
+
+    }
+}
