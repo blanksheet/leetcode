f3fcbec287805a40307b58f20226a7da41fe3e10
Tianze <547699302@qq.com>
2018-12-30 18:05:59 +0800

    Add files via upload

--- trap.java
+++ trap.java
@@ -29,8 +29,37 @@ public class trap {
        return result;
     }
 
+    public static int trap2(int[] height){
+        if(height == null || height.length == 0){
+            return 0;
+        }
+        int result = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while(left < right){
+            int min = Math.min(height[left], height[right]);
+
+            if(height[left] == min){
+                left++;
+                while (left < right && height[left] < min){
+                    result += min - height[left];
+                    left++;
+                }
+            }
+            else {
+                right--;
+                while (left < right && height[right] < min){
+                    result += min - height[right];
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args){
         int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println(trap(height));
+        System.out.println(trap2(height));
     }
 }
