8b99ebd361429811c13a2f4426f875397eb78d97
Tianze <547699302@qq.com>
2019-01-08 16:00:54 +0800

    Add files via upload

---
+++ 剑指Offer/GetNext.java
@@ -0,0 +1,40 @@
+/**
+ * Created by tianze
+ * 2019/1/8 15:50
+ */
+public class GetNext {
+    public class TreeLinkNode {
+        int val;
+        TreeLinkNode left = null;
+        TreeLinkNode right = null;
+        TreeLinkNode next = null;
+
+        TreeLinkNode(int val) {
+            this.val = val;
+        }
+    }
+    public TreeLinkNode GetNext(TreeLinkNode pNode)
+    {
+        if(pNode == null){
+            return null;
+        }
+
+        if(pNode.right != null){
+            pNode = pNode.right;
+
+            while (pNode.left !=null){
+                pNode = pNode.left;
+            }
+            return pNode;
+        }
+        while (pNode.next != null){
+            if (pNode.next.left == pNode){
+                return pNode.next;
+            }
+            pNode = pNode.next;
+        }
+
+        return null;
+
+    }
+}
