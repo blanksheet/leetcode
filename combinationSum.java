823337ead338bcde732dd6b5acf6cc4cffccd5bc
Tianze <547699302@qq.com>
2019-01-19 16:45:11 +0800

    Add files via upload

---
+++ combinationSum.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by tianze
+ * 2019/1/19 16:06
+ */
+public class combinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(candidates == null || candidates.length == 0){
+            return result;
+        }
+
+        Arrays.sort(candidates);
+
+        helper(candidates, 0, target, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    private void helper(int[] candidates, int start, int target,ArrayList<Integer> item, List<List<Integer>> result){
+        if(target < 0){
+            return;
+        }
+
+        if(target == 0){
+            result.add(new ArrayList<Integer>(item));
+            return;
+        }
+
+        for(int i = start; i < candidates.length; i++){
+            item.add(candidates[i]);
+            helper(candidates, i, target - candidates[i], item, result);
+            item.remove(item.size() - 1);
+        }
+    }
+}
