b352a1d6f305d2af35dc5fb1f7febe3d4708d3a1
Tianze <547699302@qq.com>
2019-03-12 23:29:48 +0800

    Add files via upload

--- permute.java
+++ permute.java
@@ -13,32 +13,32 @@ public class permute {
             return result;
         }
 
-        ArrayList<Integer> data = new ArrayList<>();
+        //helper(new ArrayList<>(), 0, nums.length, result, data);
 
-        for(int i : nums){
-            data.add(i);
-        }
-
-        helper(new ArrayList<>(), 0, nums.length, result, data);
-
+        boolean[] flag = new boolean[nums.length];
+        helper(new ArrayList<Integer>(), 0, nums, flag,result);
         return result;
 
 
     }
 
-    private static void helper(ArrayList<Integer> temp, int index, int length, List<List<Integer>> result, ArrayList<Integer> data){
-        if(index == length){
+    private static void helper(ArrayList<Integer> temp, int index, int[] nums, boolean[] flag, List<List<Integer>> result){
+        if(index == nums.length){
             result.add(new ArrayList<>(temp));
             return;
         }
 
-        for(int i = 0; i < data.size(); i++){
-            int tempInt = data.get(i);
-            temp.add(tempInt);
-            data.remove(i);
-            helper(temp,index + 1, length, result, data);
-            data.add(tempInt);
-            temp.remove(temp.size() - 1);
+        for(int i = 0; i < nums.length; i++){
+            if(!flag[i]){
+                flag[i] = true;
+                temp.add(nums[i]);
+                helper(temp, index + 1, nums, flag, result);
+                temp.remove(temp.size() - 1);
+                flag[i] = false;
+            }
+            //else {
+            //    continue;
+            //}
         }
     }
 
